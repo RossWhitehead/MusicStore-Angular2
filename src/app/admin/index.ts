d49714ba145702a00166529e925a512bf0bfb6b1
Ross Whitehead <rossshaggywhitehead@hotmail.com>
2017-05-31 17:19:02 +0100

    Merge again

--- src/app/admin/index.ts
+++ src/app/admin/index.ts
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 export * from './admin.module';
 export * from './admin.component';
-=======
-export * from './admin.component';
-export * from './admin.module';
->>>>>>> 5d100e04c7ef7186419d73529fd76b7bb920e4d4
