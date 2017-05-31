5c36827d7d49e44f596a6897b282059f38f4e76a
Ross Whitehead <rossshaggywhitehead@hotmail.com>
2017-05-31 16:52:50 +0100

    Add firebase config to app config

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -20,7 +20,12 @@ export const MUSIC_STORE_APP_CONFIG: AppConfig = {
     browse: 'browse/:name'
   },
   firebaseConfig: {
-
+    apiKey: 'AIzaSyCIStFdv9CPHAVPu6MAMll0xtivhr9exqU',
+    authDomain: 'musicstore-470a5.firebaseapp.com',
+    databaseURL: 'https://musicstore-470a5.firebaseio.com',
+    projectId: 'musicstore-470a5',
+    storageBucket: 'musicstore-470a5.appspot.com',
+    messagingSenderId: '285354500382'
   }
 };
 
